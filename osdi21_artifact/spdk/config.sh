2a0feb2ac64ec7b555d79f521a7e5478968abb50
Midhul <midhul.v@gmail.com>
2021-04-02 13:51:26 -0400

    Update

--- osdi21_artifact/spdk/config.sh
+++ osdi21_artifact/spdk/config.sh
@@ -12,6 +12,6 @@ IP_ADDR="192.168.10.115"
 # One way to look for SSDs is to run `lspci | grep -i nvme`
 # For example on our machine, the output of this is as follows:
 # `dc:00.0 Non-Volatile memory controller: Samsung Electronics Co Ltd NVMe SSD Controller 172Xa/172Xb (rev 01)`
-# Here the first column is showing the SSDs PCIe address. dc:00.0 => 0000:db:00.0 (you need to add the zeros in front)
+# Here the first column is showing the SSDs PCIe address. dc:00.0 => 0000:dc:00.0 (you need to add the zeros in front)
 #SSD_ADDR="none"
 SSD_ADDR="0000:dc:00.0"
