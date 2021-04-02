8f5d8c1d4f074d33b736e63100573de69e90a026
Midhul <midhul.v@gmail.com>
2021-04-02 17:41:38 -0400

    Fig 8

---
+++ osdi21_artifact/caladan/caladan_fig8.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Example usage: ./run_varyload.sh varyload-remoteram 192.168.10.115:5000 192.168.10.116:5000 60 $((32*1024)) 2
+
+config=$1
+thru_target=$2
+lat_target=$3
+duration=$4
+thru_sz=$5
+thru_qd=$6
+lat_sz=$((4*1024))
+lat_qd=1
+use_cores="0,4,8,12,16,20"
+
+# Warmup
+echo "Starting warmup"
+./run_apps.sh $config-warmup $thru_target $lat_target $duration $thru_sz $thru_qd 6 0 $use_cores;
+
+for qd in 1 2 4 8 16 32; do
+
+    echo "Starting $config-qd$qd";
+    ./run_apps.sh $config-qd$qd $thru_target $lat_target $duration $thru_sz $qd 6 6 $use_cores;
+    sleep 10;
+
+done
