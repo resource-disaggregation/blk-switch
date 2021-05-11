1ef987c64708ed6d5c34f9ce05a7d6d7f8c9fe04
Midhul <midhul.v@gmail.com>
2021-05-11 14:28:09 -0400

    Niceness val for l-apps

---
+++ osdi21_artifact/spdk/collect_varynice.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+prefix=results/varynice;
+num_cores=1;
+
+# echo "lat-apps iops thru latavg lattail latiops"
+# for num_lat in 1 2 4 8; do
+#     paste <(echo $num_lat) <(cat $prefix-nice$lat_nice.thru*.txt | grep "Total        " | awk '{x += $3;} END {print x, x*128*1024*8/1e9;}') <(cat $prefix-nice$lat_nice.lat*.txt | grep "Total        " | awk 'BEGIN{x=0;} {x += $5;} END{print x/NR;}') <(cat $prefix-nice$lat_nice.lat*.txt | grep "99.00000" | tr -d 'us' | awk 'BEGIN{x=0;} {x += $3;} END{print x/NR;}') <(cat $prefix-nice$lat_nice.lat*.txt | grep "Total        " | awk '{x += $3;} END {print x, x*4*1024*8/1e9;}');
+# done
+# echo ""
+
+echo "L-apps average-latency tail-latency";
+for lat_nice in 0 -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 -13 -14 -15 -16 -17 -18 -19 -20; do
+    paste <(echo $lat_nice <(cat $prefix-nice$lat_nice.lat*.txt | grep "Total        " | awk 'BEGIN{x=0;} {x += $5;} END{print x/NR;}') <(cat $prefix-nice$lat_nice.lat*.txt | grep "99.00000" | tr -d 'us' | awk 'BEGIN{x=0;} {x += $3;} END{print x/NR;}');
+done
+echo ""
+
+
+echo "T-apps total-throughput throughput-per-core"
+for lat_nice in 0 -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 -13 -14 -15 -16 -17 -18 -19 -20; do
+    tapp_xput=$(cat $prefix-nice$lat_nice.thru*.txt | grep "Total        " | awk '{x += $3;} END {print x*128*1024*8/1e9;}');
+    lapp_xput=$(cat $prefix-nice$lat_nice.lat*.txt | grep "Total        " | awk '{x += $3;} END {print x*4*1024*8/1e9;}');
+    paste <(echo $lat_nice <(awk -v txp=$tapp_xput -v lxp=$lapp_xput -v n=$num_cores 'BEGIN {print (txp), (txp)/n}');
+done
